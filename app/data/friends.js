af2ba40c81ac035713be1af43a3e68691e54aff7
aujones12 <46331537+aujones12@users.noreply.github.com>
2019-06-10 14:33:41 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+let friends = [
+{
+  "name":"Steve",
+  "photo":"http://www.gstatic.com/tv/thumb/persons/290995/290995_v9_ba.jpg",
+  "scores":[
+      5,
+      4,
+      1,
+      5,
+      1
+    ] 
+},
+{
+  "name":"Jenna",
+  "photo":"http://www.gstatic.com/tv/thumb/persons/248006/248006_v9_bb.jpg",
+  "scores":[
+      1,
+      5,
+      5,
+      5,
+      3
+    ]
+},
+{
+  "name":"John",
+  "photo":"http://www.gstatic.com/tv/thumb/persons/308909/308909_v9_bb.jpg",
+  "scores":[
+      1,
+      3,
+      3,
+      1,
+      5
+    ]
+},
+{
+  "name":"Rainn",
+  "photo":"http://www.gstatic.com/tv/thumb/persons/222248/222248_v9_bb.jpg",
+  "scores":[
+      1,
+      4,
+      3,
+      1,
+      5
+    ]
+},
+{
+  "name":"Angela",
+  "photo":"http://www.gstatic.com/tv/thumb/persons/280873/280873_v9_bb.jpg",
+  "scores":[
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+},
+{
+  "name":"Ed",
+  "photo":"http://www.gstatic.com/tv/thumb/persons/274723/274723_v9_bb.jpg",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+}
+];
+
+module.exports = friends;
+
+
